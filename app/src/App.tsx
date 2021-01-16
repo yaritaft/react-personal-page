d45c99db9fcafe3afbdbc2d60749c74603edbe1b
Yari Taft <yari.taft@vindow.com>
2021-01-16 18:43:13 -0300

    Components made route added.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { FormikForm } from './Components/ToggleWithoutHandlerPassed';
+import { Contact } from './Components/Contact';
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { Home } from './Components/Home';
+import { Blog } from './Components/Blog';
+import { About } from './Components/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <FormikForm />
+      <BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path="/contact">
+          <Contact />
+          </Route>
+          <Route exact path="/">
+          <Home />
+          </Route>
+          <Route exact path="/blog">
+          <Blog />
+          </Route>
+          <Route exact path="/about">
+          <About />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
       </header>
     </div>
   );
---
+++ app/src/Components/Blog.tsx
@@ -0,0 +1,19 @@
+import 'react-app-polyfill/ie11';
+import * as React from 'react';
+import { Formik, Field, Form, FormikHelpers, useFormikContext } from 'formik';
+
+interface Values {
+  firstName: string;
+  lastName: string;
+  email: string;
+}
+
+export const Blog = () => {
+//   const formik  = useFormikContext();
+const { values, submitForm } = useFormikContext();
+//   const values = formik?.values as Values;
+  return (
+      <>
+      </>
+  );
+};
