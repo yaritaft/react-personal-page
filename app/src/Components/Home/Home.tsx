b402d32c49c4408e2fb63309d21a66d2a8055498
Yari Taft <yari.taft@vindow.com>
2021-01-29 04:30:29 -0300

    Removed md.

--- app/src/Components/Home/Home.tsx
+++ app/src/Components/Home/Home.tsx
@@ -1,5 +1,7 @@
 import "react-app-polyfill/ie11";
 import * as React from "react";
+import { Center, Image } from "@chakra-ui/react";
+
 // import { Link } from 'react-router-dom';
 // import { About } from '../About/About';
 
@@ -7,7 +9,8 @@ export const Home = () => {
   //   const formik  = useFormikContext();
   //   const values = formik?.values as Values;
   return (
-    <>
-    </>
+    <Center h="400px">
+      <Image src="https://bit.ly/sage-adebayo" borderRadius="full" boxSize="150px" />
+    </Center>
   );
 };
